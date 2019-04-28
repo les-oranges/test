c2585098a44ab94619b68d33c890f589d24927b7
les-oranges <m18280261691@163.com>
2019-04-28 21:19:28 +0800

    build

---
+++ build/precache-manifest.8e3a25e7a48121dd306995768e5f6eeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bd303ab8d7a0a157bcc3",
+    "url": "/static/js/main.bd303ab8.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bd303ab8d7a0a157bcc3",
+    "url": "/static/css/main.07e7b1dd.chunk.css"
+  },
+  {
+    "revision": "0185122cd09adb8c9582150623195a74",
+    "url": "/index.html"
+  }
+];
